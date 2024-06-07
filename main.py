83916ee4f588272dc742cf2c16d454936c32467a
natalie <nataliekamarytova@seznam.cz>
2024-06-07 23:13:49 +0200

    enumy

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from kocour.databaze import pridej_vahu
 from kocour.vypocty import prum_prirustek 
 import sqlite3
 from datetime import datetime
+from enum import Enum
+
+class Moznosti(Enum):
+    AKTUALNI_VAHA = 1
+    HISTORICKA_VAHA = 2
+    VAHOVY_NARUST = 3
+    PRUM_PRIRUSTEK = 4
+    MOZNOSTI = 5
+    ZAVRIT = 6
 
 connection = sqlite3.connect("kocour.db")
 
@@ -10,26 +19,28 @@ cursor.execute("CREATE TABLE if not exists vahy (datum TEXT, vaha REAL)")
 
 
 
-UVOD =(""" Ahoj, vitej v nasi aplikaci. Muzes zde vybirat z nekolika funkci.
-1 - Zadat aktuální vahu kocoura.
-2 - Zadat historickou vahu kocoura.
-3 - Vypsat postupny vahovy narust.
-4 - Spocitat prumerny prirustek kocoura za den.
-5 - Vypsat znovu moznosti.
-6 - Zavrit aplikaci """)
+UVOD =(f""" Ahoj, vitej v nasi aplikaci. Muzes zde vybirat z nekolika funkci.
+{Moznosti.AKTUALNI_VAHA.value} - Zadat aktuální vahu kocoura.
+{Moznosti.HISTORICKA_VAHA.value} - Zadat historickou vahu kocoura.
+{Moznosti.VAHOVY_NARUST.value} - Vypsat postupny vahovy narust.
+{Moznosti.PRUM_PRIRUSTEK.value} - Spocitat prumerny prirustek kocoura za den.
+{Moznosti.MOZNOSTI.value} - Vypsat znovu moznosti.
+{Moznosti.ZAVRIT.value} - Zavrit aplikaci """)
 print(UVOD) 
 
 while True:
     vyber = input('Vyber co chces udelat')
-    if vyber == '1':
+    if vyber == str(Moznosti.AKTUALNI_VAHA.value):
         vaha = input('Zadej vahu kocoura:')
         from datetime import date
         datum = date.today() 
         cursor.execute("INSERT INTO vahy VALUES (?, ?)",(str(datum), float(vaha)))
         connection.commit()
-    elif vyber == '3':
+    elif vyber == str(Moznosti.HISTORICKA_VAHA.value):
         pass
-    elif vyber == '4':
+    elif vyber == str(Moznosti.VAHOVY_NARUST.value):
+        pass
+    elif vyber == str(Moznosti.PRUM_PRIRUSTEK.value):
         vahy = []
         datumy = []
         rows = cursor.execute("SELECT * FROM vahy").fetchall()
@@ -45,9 +56,9 @@ while True:
 
         denni_prirustek = prirustek/rozdíl
         print(denni_prirustek)
-    elif vyber == '5':
+    elif vyber == str(Moznosti.MOZNOSTI.value):
         print(UVOD)
-    elif vyber == '6':
+    elif vyber == str(Moznosti.ZAVRIT.value):
         break
     break
 
